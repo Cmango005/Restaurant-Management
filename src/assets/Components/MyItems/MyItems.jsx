2d0e6fc98cab409168c2b5efec2ab8241688e01b
Cmango005 <mh410676@gmail.com>
2023-11-09 20:46:36 +0600

    my ordered foods commit

--- src/assets/Components/MyItems/MyItems.jsx
+++ src/assets/Components/MyItems/MyItems.jsx
@@ -3,15 +3,15 @@ import { useContext, useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import './MyItems.css'
 const MyItems = () => {
-    const {user}=useContext(AuthContext);
-    const [order,setOrder]= useState([])
-    
+    const { user } = useContext(AuthContext);
+    const [order, setOrder] = useState([])
+
     const url = `http://localhost:5000/order?email=${user?.email}`
-    useEffect(()=>{
+    useEffect(() => {
         fetch(url)
-        .then(res=> res.json())
-        .then(data => setOrder(data))
-    },[url])
+            .then(res => res.json())
+            .then(data => setOrder(data))
+    }, [url])
     return (
         <div>
             <Navbar></Navbar>
@@ -19,14 +19,13 @@ const MyItems = () => {
             <div className="mt-28 grid grid-cols-2">
                 {
                     order.map(i => <div className="flex justify-around" key={i._id}>
-                       <img className="w-64 h-80 rounded-lg" src={i.FoodImage} alt="" /> 
-                       <div className="space-y-4">
-                        <p>Ordered By: {i.email}</p>
-                         <p>Food Name: {i.FoodName}</p>
-                        
-                         <p>Price: {i.Price}</p>
-                         <button className="unique-button">Cancel Order</button>
-                       </div>
+                        <img className="w-64 h-80 rounded-lg" src={i.FoodImage} alt="" />
+                        <div className="space-y-4">
+                            <p className="text-white font-bold text-lg">Ordered By: {i.email}</p>
+                            <p className="text-white font-bold text-lg">Food Name: {i.FoodName}</p>
+                            <p className="text-white font-bold text-lg">Price: {i.Price}</p>
+                            <button className="unique-button">Cancel Order</button>
+                        </div>
                     </div>)
                 }
             </div>
